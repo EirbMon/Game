0bfe35d20104ea0ebed9a0cd07c0b42b2fc1c166
Valerian Malay <valmalay@gmail.com>
2019-12-02 18:47:44 +0100

    Build with less CPU usage

--- Unity/Assets/Scripts/GameManager.cs
+++ Unity/Assets/Scripts/GameManager.cs
@@ -29,22 +29,5 @@ public class GameManager : NetworkBehaviour
             Debug.Log("DoInteraction fail");
         }
     }
-
-    public void GenerateFirstPokemon (string JSONString) {
-
-        var PokemonsJSON = JSON.Parse(JSONString);
-        int N = PokemonsJSON.Count;
-        Debug.Log("Genetaring " + N + " Server Pokemon");
-                
-        for (int i=0; i<N; i++){    
-            float pos_x = float.Parse(PokemonsJSON[i]["position_x"],CultureInfo.InvariantCulture.NumberFormat);
-            float pos_y = float.Parse(PokemonsJSON[i]["position_y"],CultureInfo.InvariantCulture.NumberFormat);
-            var Pokemon = (GameObject)Instantiate(Resources.Load(PokemonsJSON[i]["type"], typeof(GameObject)), new Vector2(pos_x, pos_y), Quaternion.identity) as GameObject;
-            Pokemon.name = "StarterPokemon"+i;
-            Pokemon.GetComponent<PokemonObject>().Initiate(PokemonsJSON[i]);
-            NetworkServer.Spawn(Pokemon);
-
-        }
-
-    }
+    
 }
--- Unity/Assets/Scripts/ServerManager.cs
+++ Unity/Assets/Scripts/ServerManager.cs
@@ -12,11 +12,27 @@ public class ServerManager : NetworkBehaviour
     // Start is called before the first frame update
     public override void OnStartServer()
     {     
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Application.targetFrameRate = 60;
         string JSONString = "[{\"type\":\"Pikachu\",\"name\":\"PikaPika\",\"color\":\"yellow\",\"position_x\":\"-56.5\",\"position_y\":\"3.6\"},{\"type\":\"Carapuce\",\"name\":\"CaraCara\",\"color\":\"blue\",\"position_x\":\"-57.5\",\"position_y\":\"3.6\"},{\"type\":\"Salameche\",\"name\":\"SalaSala\",\"color\":\"red\",\"position_x\":\"-55.5\",\"position_y\":\"3.6\"}]";
         //gameManager.SendMessageToReact("starter_pokemon");
-        gameManager.GenerateFirstPokemon(JSONString);
+        GenerateFirstPokemon(JSONString);
         
+    }
+
+    public void GenerateFirstPokemon (string JSONString) {
+
+        var PokemonsJSON = JSON.Parse(JSONString);
+        int N = PokemonsJSON.Count;
+        Debug.Log("Genetaring " + N + " Server Pokemon");
+                
+        for (int i=0; i<N; i++){    
+            float pos_x = float.Parse(PokemonsJSON[i]["position_x"],CultureInfo.InvariantCulture.NumberFormat);
+            float pos_y = float.Parse(PokemonsJSON[i]["position_y"],CultureInfo.InvariantCulture.NumberFormat);
+            var Pokemon = (GameObject)Instantiate(Resources.Load(PokemonsJSON[i]["type"], typeof(GameObject)), new Vector2(pos_x, pos_y), Quaternion.identity) as GameObject;
+            Pokemon.name = "StarterPokemon"+i;
+            Pokemon.GetComponent<PokemonObject>().Initiate(PokemonsJSON[i]);
+            NetworkServer.Spawn(Pokemon);
+        }
     }
 
 }
